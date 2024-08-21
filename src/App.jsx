a6d176ae77a93b827273778cd873b5cdec16c731
samcq2 <108625155+samcq2@users.noreply.github.com>
2024-08-21 17:59:09 +0100

    file updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from '../Components/Home/Home.jsx';
+import Overview from '../Components/Overview/Overview.jsx';
 import Layout from '../Components/Layout/Layout.jsx';
+
 
 const App = () => (
   <Router>
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
+        <Route index element={<Overview />} />
+            
       </Route>
     </Routes>
   </Router>
