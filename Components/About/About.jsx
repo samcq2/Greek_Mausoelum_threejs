a2454ac3d111bca307935442ece38d4fea5b75a0
samcq2 <108625155+samcq2@users.noreply.github.com>
2024-10-10 15:27:47 +0100

    drop down about

--- Components/About/About.jsx
+++ Components/About/About.jsx
@@ -5,6 +5,14 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 const About = () => {
+
+    const [isOpen1, setIsOpen1] = useState(false);
+    const [isOpen2, setIsOpen2] = useState(false);
+    const [activeButton, setActiveButton] = useState(null);
+    const [activeButton2, setActiveButton2] = useState(null);
+    const [isHovered1, setIsHovered1] = useState(false);
+    const [isHovered2, setIsHovered2] = useState(false);
+
     useEffect(() => {
       const scene = new THREE.Scene();
       const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -83,79 +91,117 @@ const About = () => {
       };
     }, []);
 
-    return (
-        <div className="Desc-container">
-          <div id="name">
+    const toggleDropdown = () => {
+        setIsOpen1(!isOpen1);
+        setActiveButton(activeButton === 'education' ? null : 'education');
+      };
+    
+    const toggleDropdown2 = () => {
+        setIsOpen2(!isOpen2);
+        setActiveButton2(activeButton2 === 'employment' ? null : 'employment');
+      };
 
-            Samuel Clarke-Quy
 
-          </div>
-          <div id='welcome'>
-            Welcome to my portfolio website developed using React and ThreeJS!
-          </div>
-          <div id='education-and-employment-intro'>
-            My name is Samuel Clarke-Quy a Physics with Particle Physics student.
-            I am currently employed by Ten10 as a Trainee Junior Technical Consultant.
-          </div>
 
-          <div id='education1'>
-
-            At Queen Mary University of London I graduated with a bachelors of science
-            in Physics with Particle Physcis (BSc Physics with Particle Physics).
-
-          </div>
-
-          <div id='education7'>
+      return (
+        <div className="desc-container">
             
-            Physics as a degree is always something I have been passionate about from 
-            the world of Quantum Mechanics with applications in Quantum computing for the next
-            step in cumputing power to the use of data science techniques in Particle Physics.
+            <div id="name">
+                Samuel Clarke-Quy
+            </div>
+            <div id='welcome'>
+                Welcome to my portfolio website developed using React and ThreeJS!
+            </div>
+            <div id='education-and-employment-intro'>
+                My name is Samuel Clarke-Quy a Physics with Particle Physics graduate.
+                I am currently employed by Ten10 as a Trainee Junior Technical Consultant.
+            </div>
 
-          </div>
+            <div className="button-container">
 
-          <div id='education8'>
-            
-            By pursuing a specialisation in Particle Physics I was able to learn and apply many 
-            concepts in data science in assignments and projects including my dissertation.
+                <button className="dropdown1" onClick={toggleDropdown}
+                onMouseEnter={() => setIsHovered1(true)}
+                onMouseLeave={() => setIsHovered1(false)}
+                style={{
+                    backgroundColor: activeButton === 'education' ? '#e7bd42' : isHovered1 ? '#e7bd42' : 'transparent',
+                    color: activeButton === 'education' ? '#336cce' : isHovered1 ? '#336cce' : '#f2e7bf',                  
+                  }}>
+                    {isOpen1 ? "Education" : "Education"}
+                </button>
 
-          </div>
+                <div class="divider">
 
-          <div id='education2'>
+                </div>
 
-            This included learning Python using Pandas to interact with and clean dataframes, as well as using libraries
-            to visualise and analyse segments of the dataframes using libraries such as MatPlotLib and
-            SeaBorn. Additionally at my time at univeristy I was able to learn and use R Language to 
-            visualise and interact with data frames.
+                <button className="dropdown2" onClick={toggleDropdown2}
+                onMouseEnter={() => setIsHovered2(true)}
+                onMouseLeave={() => setIsHovered2(false)}
+                style={{
+                    backgroundColor: activeButton2 === 'employment' ? '#e7bd42' : isHovered2 ? '#e7bd42' : 'transparent',
+                    color: activeButton2 === 'employment' ? '#336cce' : isHovered2 ? '#336cce' : '#f2e7bf',  
+                    
+                  }}>
+                    {isOpen2 ? "Employment": "Employment"}
+                </button>
 
-          </div>
+            </div>
+          
+          {isOpen1 && (
+            <div className="button1-container">
+              <div id='education-title'>
+                Education
+              </div>
+    
+              <div id='education1'>
+                At Queen Mary University of London I graduated with a bachelors of science in Physics with Particle Physics (BSc Physics with Particle Physics).
+              </div>
+    
+              <div id='education7'>
+                Physics as a degree is always something I have been passionate about from the world of Quantum Mechanics with applications in Quantum computing for the next step in computing power to the use of data science techniques in Particle Physics.
+              </div>
+    
+              <div id='education8'>
+                By pursuing a specialisation in Particle Physics I was able to learn and apply many concepts in data science in assignments and projects including my dissertation.
+              </div>
+    
+              <div id='education2'>
+                This included learning Python using Pandas to interact with and clean dataframes, as well as using libraries to visualise and analyse segments of the dataframes using libraries such as MatPlotLib and SeaBorn. Additionally at my time at university I was able to learn and use R Language to visualise and interact with data frames.
+              </div>
+    
+              <div id='education3'>
+                Another step in my up-skilling in data science at university was the use machine learning techniques such as Decision Trees, Logistic Regression, Naives Bayes machine learning models to analyse and produce hypotheses from data analysis performed on various dataframes.
+              </div>
+    
+              <div id='education4'>
+                Some additional analytical techniques I developed at university included Principal Component Analysis, cross-validation and Time Series Analysis.
+              </div>
+    
+              <div id='education5'>
+                My dissertation topic was on Using Machine Learning Techniques to classify and search for products of Neutrinos at DUNE.
+              </div>
+    
+              <div id='education6'>
+                Another project at University was performing data analysis on Mexico City sensors for air pollutants and levels of traffic intensity in different areas to find correlations between the two.
+              </div>
+            </div>
+          )}
 
-          <div id='education3'>
+          {isOpen2 && (
+            <div className="button2-container">
+                <div id='employment-title'>
+                    Employment
+                </div>
 
-            Another step in my up-skilling in data science at university was the use machine learning techniques
-            such Decision Trees, Logistic Regression, Naives Bayes machine learning models to analyse and produce 
-            hypothesis from data analysis performed on various dataframes.
+                <div id='employment1'>
+                    In my employment at Ten10 I was trained in a variety of areas within the technology industry including
+                    business analysis, manual testing, automated testing, development and data.
+                    This included languages such as Java, JavaScript, Python and SQL  with code editors such as 
+                    SQL Lite Studio, Oracle with SQL Developer, IntelliJ and VSCode.
+                </div>
+            </div>
 
-          </div>
 
-          <div id='education4'>
-
-            Some additional analytical techniques I developed at university included
-            Principal Component Analysis, cross-validation and Time Series Analysis.
-
-          </div>
-
-          <div id='education5'>
-            
-            My disseration topic was on Using Machine Learning Techniques to classify and search for products of
-            Neutrinos at DUNE.
-
-          </div>
-
-          <div id='education6'>
-            Another project at University was performing data analysis on Mexico City sensors for air pollutants and 
-            levels of traffic intensity in different areas to find correlations between the two.
-          </div>
-
+          )}
 
 
         </div>
