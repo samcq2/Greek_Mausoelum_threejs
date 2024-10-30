d5deb2e04fcddddcf171b1afb3bbcc714fb3c0bf
samcq2 <108625155+samcq2@users.noreply.github.com>
2024-10-30 17:17:49 +0000

    projects template

--- Components/Projects/Projects.jsx
+++ Components/Projects/Projects.jsx
@@ -6,14 +6,14 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 const About = () => {
 
-    const [isOpen1, setIsOpen1] = useState(false);
-    const [isOpen2, setIsOpen2] = useState(false);
-    const [activeButton, setActiveButton] = useState(null);
-    const [activeButton2, setActiveButton2] = useState(null);
-    const [isHovered1, setIsHovered1] = useState(false);
-    const [isHovered2, setIsHovered2] = useState(false);
-    const toggleArrow = document.getElementById("arrow");
-    const toggleArrow2 = document.getElementById("arrow2");
+    const [isOpen3, setIsOpen3] = useState(false);
+    const [isOpen4, setIsOpen4] = useState(false);
+    const [activeButton3, setActiveButton3] = useState(null);
+    const [activeButton4, setActiveButton4] = useState(null);
+    const [isHovered3, setIsHovered3] = useState(false);
+    const [isHovered4, setIsHovered4] = useState(false);
+    const toggleArrow3 = document.getElementById("arrow3");
+    const toggleArrow4 = document.getElementById("arrow4");
 
     useEffect(() => {
       const scene = new THREE.Scene();
@@ -93,7 +93,96 @@ const About = () => {
       };
     }, []);
 
+    const toggleDropdown3 = () => {
+      toggleArrow3.classList.toggle("arrow3");
+      setIsOpen3(!isOpen3);
+      setActiveButton3(activeButton3 === 'Warwick Bandsoc Web App' ? null : 'Warwick Bandsoc Web App');
     };
+    
+    const toggleDropdown4 = () => {
+      setIsOpen4(!isOpen4);
+      setActiveButton4(activeButton4 === 'Original Portfolio Website' ? null : 'Original Portfolio Website');
+      toggleArrow4.classList.toggle("arrow4");
+    };
+
+    return (
+      <div className='wrapper-container'>
+
+<div className="button-container-p">
+
+<button className="dropdown_p1" onClick={toggleDropdown3}
+onMouseEnter={() => setIsHovered3(true)}
+onMouseLeave={() => setIsHovered3(false)}
+
+style={{
+    backgroundColor: activeButton3 === 'Warwick Bandsoc Web App' ? '#e7bd42' : isHovered3 ? '#e7bd42' : 'transparent',
+    color: activeButton3 === 'Warwick Bandsoc Web App' ? '#336cce' : isHovered3 ? '#336cce' : '#f2e7bf',                  
+  }}>
+    {isOpen3 ? "Warwick Bandsoc Web App" : "Warwick Bandsoc Web App"}
+    <i3 class="bx bx-chevron-down" id="arrow3"></i3>
+</button>
+
+<div class="divider2">
+
+</div>
+
+<button className="dropdown_p2" onClick={toggleDropdown4}
+onMouseEnter={() => setIsHovered4(true)}
+onMouseLeave={() => setIsHovered4(false)}
+style={{
+    backgroundColor: activeButton4 === 'Original Portfolio Website' ? '#e7bd42' : isHovered4 ? '#e7bd42' : 'transparent',
+    color: activeButton4 === 'Original Portfolio Website' ? '#336cce' : isHovered4 ? '#336cce' : '#f2e7bf',  
+    
+  }}>
+    {isOpen4 ? "Original Portfolio Website": "Original Portfolio Website"}
+    <i4 class="bx bx-chevron-down" id="arrow4"></i4>
+</button>
+
+</div>
+      
+      <div className="desc-container2">
+          
+          <div id="title">
+              Technical Projects Portfolio
+          </div>
+        
+        {isOpen3 && (
+          <div className="button3-container">
+            <div id='project1-title'>
+              Full-stack booking page developed for Warwick Band Society
+            </div>
+  
+            <div id='project1-1'>
+              This is a full-stack app for Warwick Band Society developed as per the requirements
+              of the Bandsoc executives for a booking page which allows for automated booking 
+              and integrated payment through the Stripe API.
+            </div>
+          </div>
+        )}
+
+        {isOpen4 && (
+          <div className="button4-container">
+              <div id='project2-title'>
+                  Employment
+              </div>
+
+              <div id='project2-1'>
+                  In my employment at Ten10 I was trained in a variety of areas within the technology industry including
+                  business analysis, manual testing, automated testing, development and data.
+                  This included languages such as Java, JavaScript, Python and SQL  with code editors such as 
+                  SQL Lite Studio, Oracle with SQL Developer, IntelliJ and VSCode.
+              </div>
+          </div>
+
+
+        )}
+      </div>
+      </div>
+    );
+
+    };
+
+
 
     
   
