4696eab6cc49e9ad4a1e8ba278c0f8a62b23d4e3
samcq2 <108625155+samcq2@users.noreply.github.com>
2024-11-06 14:21:14 +0000

    config vite

--- Front-end/vite.config.js
+++ Front-end/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Proxy API requests to the backend
+      '/api': 'http://localhost:5000', // Update this if your backend runs on a different port
+    },
+  },
 });
