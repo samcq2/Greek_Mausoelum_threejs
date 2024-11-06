56138152265a9f754408c073ff4964af49fdb066
samcq2 <108625155+samcq2@users.noreply.github.com>
2024-11-06 14:40:30 +0000

    pls work slso

--- Front-end/Components/Contact/Contact.jsx
+++ Front-end/Components/Contact/Contact.jsx
@@ -124,7 +124,7 @@ const Contact = () => {
         };
 
         try {
-            let response = await fetch('http://localhost:5001/send', {
+            let response = await fetch('/api/send', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json;charset=utf-8',
