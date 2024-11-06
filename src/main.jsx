60f96e7620741811e93727a350ef189c21f4fb87
Samuel Clarke-Quy <samuel.clarke-quy@centre4.local>
2024-11-06 12:35:30 +0000

    dup file delete

--- src/main.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx'; // Imports the main App component
-
-const root = ReactDOM.createRoot(document.getElementById('root')); // Target element where React will render
-root.render(<App />);
