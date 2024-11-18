3c26235833a96dcee743ff7e42464d1d2a13dc3e
samcq2 <108625155+samcq2@users.noreply.github.com>
2024-11-18 13:05:02 +0000

    navbar

--- Front-end/Components/Sidebar/Sidebar.jsx
+++ Front-end/Components/Sidebar/Sidebar.jsx
@@ -1,129 +1,63 @@
-import { Link, NavLink } from 'react-router-dom';
-import LogoS from '../../Pictures/Samuel.png';
-import LogoC from '../../Pictures/Clarke.png';
-import LogoQ from '../../Pictures/Quy.png';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCode, faHome, faEnvelope, faCircleUser, faLandmark } from '@fortawesome/free-solid-svg-icons';
+import { Link, NavLink } from 'react-router-dom'
+import './sidebar.scss'
+import LogoS from '../../Pictures/Samuel.png'
+import LogoC from '../../Pictures/Clarke.png'
+import LogoQ from '../../Pictures/Quy.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCode } from '@fortawesome/free-solid-svg-icons'
+import { faHome, faEnvelope, faCircleUser, faLandmark } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Sidebar = () => (
-  <div className="flex flex-col items-center bg-gray-900 text-gray-100 w-20 h-screen fixed left-0 md:w-24 lg:w-28">
-    <Link to="/" className="my-4">
-      <div className="flex flex-col items-center">
-        <img
-          src={LogoS}
-          alt="Logo S"
-          className="flex w-8 h-6 md:w-12 md:h-8 lg:w-14 lg:h-10 mb-2"
-        />
-        <img
-          src={LogoC}
-          alt="Logo C"
-          className="flex w-8 h-6 md:w-12 md:h-8 lg:w-14 lg:h-10 mb-2"
-        />
-        <img
-          src={LogoQ}
-          alt="Logo Q"
-          className="flex w-8 h- md:w-12 md:h- lg:w-14 lg:h-10"
-        />
-      </div>
+  <div className='nav-bar'>
+    <Link className='logo' to='\'>
+      <img src={LogoS} alt="logo" className="sub-logo"/>
+      
+      <img src={LogoC} alt="logo" className="sub-logo2"/>
+      <img src={LogoQ} alt="logo" className="sub-logo3"/>
     </Link>
+    <nav>
 
-    <nav className="flex flex-col gap-10 mt-8">
-      <NavLink
-        exact="true"
-        className={({ isActive }) =>
-          `text-gray-400 hover:text-yellow-300 ${isActive ? 'text-yellow-300' : ''}`
-        }
-        to="/"
-      >
-        <FontAwesomeIcon
-          icon={faLandmark}
-          className="flex text-2xl md:text-5xl lg:text-4xl"
-        />
-      </NavLink>
+        <NavLink exact="true" activeclassname="active" className="overview-link" to="/">
+            <FontAwesomeIcon icon={faLandmark} color="#f2e7bf" />
 
-      <NavLink
-        exact="true"
-        className={({ isActive }) =>
-          `text-gray-400 hover:text-yellow-300 ${isActive ? 'text-yellow-300' : ''}`
-        }
-        to="/home"
-      >
-        <FontAwesomeIcon
-          icon={faHome}
-          className="flex text-2xl md:text-5xl lg:text-4xl"
-        />
-      </NavLink>
+        </NavLink>
 
-      <NavLink
-        exact="true"
-        className={({ isActive }) =>
-          `text-gray-400 hover:text-yellow-300 ${isActive ? 'text-yellow-300' : ''}`
-        }
-        to="/about"
-      >
-        <FontAwesomeIcon
-          icon={faCircleUser}
-          className="flex text-2xl md:text-5xl lg:text-4xl"
-        />
-      </NavLink>
+        <NavLink exact="true" activeclassname="active" className= "home-link" to="/home">
+            <FontAwesomeIcon icon={faHome} color="#f2e7bf" />
 
-      <NavLink
-        exact="true"
-        className={({ isActive }) =>
-          `text-gray-400 hover:text-yellow-300 ${isActive ? 'text-yellow-300' : ''}`
-        }
-        to="/projects"
-      >
-        <FontAwesomeIcon
-          icon={faCode}
-          className="flex text-2xl md:text-5xl lg:text-4xl"
-        />
-      </NavLink>
+        </NavLink>
 
-      <NavLink
-        exact="true"
-        className={({ isActive }) =>
-          `text-gray-400 hover:text-yellow-300 ${isActive ? 'text-yellow-300' : ''}`
-        }
-        to="/contact"
-      >
-        <FontAwesomeIcon
-          icon={faEnvelope}
-          className="flex text-2xl md:text-5xl lg:text-4xl"
-        />
-      </NavLink>
+        <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+            <FontAwesomeIcon icon={faCircleUser} color="#f2e7bf" />
+
+        </NavLink>
+        
+        <NavLink exact="true" activeclassname="active" className="projects-link" to="/projects">
+            <FontAwesomeIcon icon={faCode} color="#f2e7bf" />
+
+        </NavLink>
+
+        <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+            <FontAwesomeIcon icon={faEnvelope} color="#f2e7bf" />
+
+        </NavLink>
     </nav>
-
-    <ul className="flex flex-col items-center gap-4 mt-auto mb-8">
+    <ul>
       <li>
-        <a
-          href="https://www.linkedin.com/in/samuelclarke-quy/"
-          target="_blank"
-          rel="noreferrer"
-          className="text-gray-400 hover:text-yellow-300"
-        >
-          <FontAwesomeIcon
-            icon={faLinkedin}
-            className="text-lg md:text-5xl lg:text-4xl"
-          />
+        <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/samuelclarke-quy/'>
+          <FontAwesomeIcon icon={faLinkedin} color="#f2e7bf" />
         </a>
       </li>
+
       <li>
-        <a
-          href="https://github.com/samcq2"
-          target="_blank"
-          rel="noreferrer"
-          className="text-gray-400 hover:text-yellow-300"
-        >
-          <FontAwesomeIcon
-            icon={faGithub}
-            className="text-lg md:text-5xl lg:text-4xl"
-          />
+        <a target="_blank" rel='noreferrer' href='https://github.com/samcq2'>
+          <FontAwesomeIcon icon={faGithub} color="#f2e7bf" />
         </a>
       </li>
     </ul>
+
   </div>
-);
+)
 
 export default Sidebar;
